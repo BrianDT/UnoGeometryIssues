2ef18571a53c1463d820b9615cc83ac0a4e80cd8
Brian Taylor <brian@vsoftsys.co.uk>
2020-04-13 17:19:18 +0100

    Demonstrate Rectangle display issue in WASM

---
+++ UnoGeometryIssues/UnoGeometryIssues.Shared/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Vssl.Samples.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoGeometryIssues
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        /// <summary>
+        /// Gets or sets the view model for the menu
+        /// </summary>
+        public MainViewModel VM
+        {
+            get
+            {
+                return this.DataContext as MainViewModel;
+            }
+        }
+
+        /// <summary>
+        /// Toggle the round button size
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The event args</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.VM.Toggle();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Vssl.Samples.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MainViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Gets a value indicating whether the button size is reduced
+        /// </summary>
+        public bool IsReduced { get; private set; }
+
+        /// <summary>
+        /// Toggle the size of the round button
+        /// </summary>
+        public void Toggle()
+        {
+            this.IsReduced = !this.IsReduced;
+            this.OnPropertyChanged("IsReduced");
+        }
+    }
+}
